242a7faf5d338eceb964f1b786d744ec76e42fb4
martialus89 <leroymartial59@gmail.com>
2024-09-11 14:56:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ st.write(df[df.ville == user_selection])
 age_selection = st.slider("Select un age", min_value=20, max_value=100, value=30, step=1)
 st.write(df[df.age == age_selection])
 
+if st.checkbox("print jeu de données"):
+  st.write(df[df.age == age_selection, df.ville == user_selection])
 
