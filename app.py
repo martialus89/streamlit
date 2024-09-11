a1c72ec6cf869c07ff616b61404656839e0e7a3a
martialus89 <leroymartial59@gmail.com>
2024-09-11 16:13:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ if uploaded_file is not None:
 
   
     user_selection = st.selectbox('Select une variable',list(dataframe.columns.values))
-    st.write(dataframe.groupby(user_selection).value_counts())
-    
+    st.write(dataframe.groupby(user_selection).value_counts()[user_selection])
+      
              
     # st.write(dataframe[user_selection])
     # chart_data = pd.DataFrame(dataframe.groupby(user_selection).count(), columns=[user_selection])
