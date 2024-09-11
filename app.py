2130f6db6c8f02d294d5d03babf72d5841df88c6
martialus89 <leroymartial59@gmail.com>
2024-09-11 16:24:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 import numpy as np
+import matplotlib.pyplot as plt
 
 uploaded_file = st.file_uploader("Choose a file",type=['csv'] )
 if uploaded_file is not None:
@@ -25,7 +26,26 @@ if uploaded_file is not None:
   
     user_selection = st.selectbox('Select une variable',list(dataframe.columns.values))
     st.write(dataframe[user_selection].value_counts())
-    chart_data = pd.DataFrame(dataframe[user_selection].value_counts(), columns=[user_selection])
-    st.bar_chart(chart_data)
+    
 
+    # Exemple pour la variable 'Ever_Married' (autopct='%1.1f%%')
+    plt.figure(figsize=(8, 8))
+    st.pyplot(dataframe[user_selection].value_counts().plot.pie(autopct='%1.1f%%',colors=['skyblue', 'lightcoral'], startangle=90))
+        # dataframe[user_selection].value_counts().plot.pie(autopct='%1.1f%%',colors=['skyblue', 'lightcoral'], startangle=90, labels=['Homme', 'Femme'])
+    # plt.title('Répartition de l\'État Matrimonial')
+    # plt.legend()
+    # plt.show()
+
+    # st.pyplot(fig1)
+
+
+# import streamlit as st
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# arr = np.random.normal(1, 1, size=100)
+# fig, ax = plt.subplots()
+# ax.hist(arr, bins=20)
+
+# st.pyplot(fig)
 
