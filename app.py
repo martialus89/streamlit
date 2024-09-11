24aefd2fa0d63942841a9750221fe431accf24e6
martialus89 <leroymartial59@gmail.com>
2024-09-11 14:52:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ if st.checkbox("print jeu de données"):
 
 ville=df.ville.unique()
 user_selection = st.selectbox('Select une ville',ville)
+st.slider("Select un age", min_value=20, max_value=100, value=30, step=1)
 st.write(df[df.ville == user_selection])
 
 
