184fe8f2ef9c45f6069595ec48c7626f5541836b
martialus89 <leroymartial59@gmail.com>
2024-09-11 15:06:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ df = pd.read_csv('data.csv', sep=';')
 
 ville=df.ville.unique()
 user_selection = st.selectbox('Select une ville',ville)
-st.write(df[df.ville == user_selection])
+# st.write(df[df.ville == user_selection])
 
 age_selection = st.slider("Select un age", min_value=20, max_value=100, value=30, step=1)
-st.write(df[df.age == age_selection])
+# st.write(df[df.age == age_selection])
 
 if st.checkbox("print jeu de données"):
   st.write(df[(df.age == age_selection)&(df.ville == user_selection)])
