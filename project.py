00b1effec645b3e4dd224afc29b1cf344d9ca90b
martialus89 <leroymartial59@gmail.com>
2024-10-03 16:02:50 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import pandas as pd
 
+st.title("My dashboard")
+st.subheader("My subtitle")
+
+df = pd.read_csv('data.csv', sep=';')
+
+# if st.checkbox("print jeu de données"):
+#   st.write(df)
+
+ville=df.ville.unique()
+user_selection = st.selectbox('Select une ville',ville)
+# st.write(df[df.ville == user_selection])
+
+age_selection = st.slider("Select un age", min_value=20, max_value=100, value=30, step=1)
+# st.write(df[df.age == age_selection])
+
+if st.checkbox("print jeu de données"):
+  st.write(df[(df.age == age_selection)&(df.ville == user_selection)])
+
+# picture = st.camera_input("Take a picture")
+
+# if picture:
+#     st.image(picture)
