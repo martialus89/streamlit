83fa3c2b757cfd79e80f4e7731292ae72a0403a6
martialus89 <leroymartial59@gmail.com>
2024-10-03 16:09:46 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,22 @@
 import streamlit as st
-import numpy as np
+import random
+import time
 
-st.title("My final project")
-st.subheader("My subtitle")
+st.title("Simple chat")
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
