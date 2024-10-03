a4cf7e6db75011cb721ce3f23782cbea69c4b84c
martialus89 <leroymartial59@gmail.com>
2024-10-03 16:24:27 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,20 +1,16 @@
 import streamlit as st
 
-st.title("Echo Bot")
+st.title("P R O J E C T")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# st.write(st.session_state.messages)
-
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Comment puis-je vous aider ???"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
@@ -27,13 +23,3 @@ if prompt := st.chat_input("What is up?"):
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
-
-# import streamlit as st
-import pandas as pd
-# import numpy as np
-
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
-)
-st.map(df)
