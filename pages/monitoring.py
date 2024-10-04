16f354a67fdd9f72b4eea8cf62bd62038a957e66
martialus89 <leroymartial59@gmail.com>
2024-10-04 16:30:46 +0200

    test

--- pages/monitoring.py
+++ pages/monitoring.py
@@ -9,17 +9,6 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-conn = sqlite3.connect('dbmodel.db')
-cursor = conn.cursor()
-# Récupérer les données de la table
-cursor.execute(f'SELECT distinct model_name FROM model')
-rows = cursor.fetchall()
-cursor.close()
-conn.close()
-options = [row[0] for row in cursor.fetchall()]
-st.sidebar.selectbox('Choisissez votre modèle', options)
-
-
 histo = st.checkbox("Voir l'historique des conversations")
 if histo:
     r=requests.get('https://36e8-34-16-180-130.ngrok-free.app/read_table?table=model')
@@ -30,7 +19,8 @@ retrain = st.checkbox("Réentrainement du model")
 if retrain:
     list_model = requests.get('https://3d92-34-16-180-130.ngrok-free.app/model')
     list_model.text
-    st.write(list_model.text)
+    options = [row[0] for row in list_model]
+    st.sidebar.selectbox('Choisissez votre modèle', options)
 
 metrics = st.checkbox("Voir les metrics d'un modèle")
 if metrics:
